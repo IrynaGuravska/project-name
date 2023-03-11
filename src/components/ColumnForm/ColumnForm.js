6b89d622d8e557de5f9cb5673e810eba22aa3eee
Iryna Guravska <irinaguru.ua@gmail.com>
2023-03-11 14:55:41 +0200

    data modification

--- src/components/ColumnForm/ColumnForm.js
+++ src/components/ColumnForm/ColumnForm.js
@@ -1,25 +1,29 @@
 import styles from './ColumnForm.module.scss';
 import { useState } from 'react';
+import { useDispatch } from 'react-redux';
 import Button from '../Button/Button';
 import TextInput from '../TextInput/TextInput';
 
-const ColumnForm = props => {
+const ColumnForm = (props) => {
+
     const [title, setTitle] = useState('');
     const [icon, setIcon] = useState('');
-    const handleSubmit = e => {
-        e.preventDefault();
-        props.action({ title: title, icon: icon });
-        setTitle('');
-        setIcon('');
-    };
 
-	return (
-        <form className={styles.columnForm} onSubmit={handleSubmit}>
-            <label>Title:</label> <TextInput type="text" value={title} onChange={e => setTitle(e.target.value)} />
-            <label>Icon:</label> <TextInput type="text" value={icon} onChange={e => setIcon(e.target.value)} />
-            <Button>Add column</Button>
-        </form>
-	);
+    const dispatch = useDispatch();
+
+    const handleSubmit = e => {
+    e.preventDefault();
+    dispatch({ type: 'ADD_COLUMN', payload: { title, icon } });
+    setTitle('');
+    setIcon('');
 };
 
+return (
+    <form className={styles.columnForm} onSubmit={handleSubmit}>            
+        <span>Title:</span> <TextInput type="text" value={title} onChange={e => setTitle(e.target.value)} />
+        <span>Icon:</span> <TextInput type="text" value={icon} onChange={e => setIcon(e.target.value)} />
+        <Button>Add column</Button>
+    </form>
+);
+};
 export default ColumnForm;
