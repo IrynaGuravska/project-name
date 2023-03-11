b850bbe38174d2245bdbf782587de89705fb1a8b
Iryna Guravska <irinaguru.ua@gmail.com>
2023-03-11 18:55:57 +0200

    Add SearchForm

--- src/redux/store.js
+++ src/redux/store.js
@@ -5,9 +5,11 @@ import shortid from 'shortid';
 const reducer = (state, action) => {
   switch(action.type) {
     case 'ADD_COLUMN':
-        return { ...state, columns: [...state.columns, {...action.payload, id: shortid() }]};
+        return { ...state, columns: [...state.columns, {id: shortid(), ...action.payload }]};
     case 'ADD_CARD':
-        return { ...state, cards: [...state.cards, {id: shortid(), ...action.payload }]};    
+        return { ...state, cards: [...state.cards, {id: shortid(), ...action.payload }]};
+    case 'UPDATE_SEARCHSTRING':
+      return { ...state, searchString: action.payload };
     default:    
         return state;
 }        
