2152a59d4703cfb8acd3dc2b688dc282dd8a525c
Iryna Guravska <irinaguru.ua@gmail.com>
2023-03-12 18:54:12 +0200

    Add router and page Not Found

---
+++ src/components/Home/Home.js
@@ -0,0 +1,16 @@
+import Hero from '../Hero/Hero.js'
+import List from '../List/List.js'
+import SearchForm from '../SearchForm/SearchForm.js'
+
+const Home = () =>{
+   return (
+      <>
+         <Hero />
+         <SearchForm />
+         <List />
+         
+      </>
+   );
+};
+
+export default Home;
