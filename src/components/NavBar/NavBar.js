8290d0fcd2effe4e64f4cb66c67638e9be4dc2d9
Iryna Guravska <irinaguru.ua@gmail.com>
2023-03-13 17:45:05 +0200

    Add active to navbar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,25 +1,24 @@
 import styles from './NavBar.module.scss'
 import React from 'react';
 import Container from '../Container/Container.js';
+import { Link, NavLink } from 'react-router-dom';
 
 const NavBar = () => {
    return (
       <nav className={styles.navbar}>
          <Container>
             <div className={styles.wrapper}>
-               <a href='/' className={styles.icon}>
-                  <span className='fa fa-tasks' /></a>
-                  <ul className={styles.navlist}>
-                     <a href='/'>
-                        <li>Home</li>
-                     </a>
-                     <a href='/favorite'>
-                        <li>Favorite</li>
-                     </a>
-                     <a href='/about'>
-                        <li>About</li>
-                     </a>
-                  </ul>
+               <Link to='/' className={styles.icon}>
+                  <span className='fa fa-tasks' />
+               </Link>
+               <ul className={styles.navlist}>
+                  <li><NavLink className={({ isActive }) => isActive ? styles.linkActive : undefined}
+                     to="/">Home</NavLink></li>
+                  <li><NavLink className={({ isActive }) => isActive ? styles.linkActive : undefined} 
+                     to="/favorite">Favorite</NavLink></li>
+                  <li><NavLink className={({ isActive }) => isActive ? styles.linkActive : undefined} 
+                     to="/about">About</NavLink></li>
+               </ul>
             </div>
          </Container>
       </nav>
